247062d29c4f2efd397b94c615f0b03ee36044a2
Susmithayaramothu6677 <47516624+Susmithayaramothu6677@users.noreply.github.com>
2020-12-01 10:18:34 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j,rows;
+printf("enter the no of rows");
+
+for(i=5;i<=n;i--)
+{
+for(j=1;j<=i;j++)
+{
+
+printf("%d",i);
+}
+printf("\n");
+}
+return 0;
+}
+
